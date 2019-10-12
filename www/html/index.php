c5d8399a51544e087d52d3f41c5fc8ab9859b9b8
kakqun <csositai@gmail.com>
2019-10-13 00:06:07 +0900

    [add] header

--- www/html/index.php
+++ www/html/index.php
@@ -1,2 +1,13 @@
-<?php
-echo "Hello zuck";
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <header>
+                <?php require_once(__DIR__. '/header.html'); ?>
+            </header>
+            <h1>HELLO ZUCK</h1>
+        </div>
+    </body>
+</html>
