a7f3653e5ad1ff8bab3ff10b073e4936bc5d0140
kakqun <csositai@gmail.com>
2019-10-16 22:29:22 +0900

    change register text

--- www/html/register.php
+++ www/html/register.php
@@ -13,6 +13,15 @@
                     <?php require_once(__DIR__. '/sidebar.html'); ?>
                 </div>
                 <div class="top-content register">
+                    <div class="title">
+                        Registration
+                    </div>
+                    <div class="text">
+                        <p>  
+                            To register for thefacebook.com, just fill in the four fields below. You will have a chance to enter </br>
+                            additional information and submit a picture once you have registered.
+                        </p>
+                    </div>
                 </div>
             </div>
                 <footer>
